cb9c34c90b6878cce61faaa2005c3164268930ea
laptop <drcyclotron@gmail.com>
2019-11-07 22:51:31 +0100

    First commit

--- src/main/java/com/javabunga/springbootexample/web/HelloWorldController.java
+++ src/main/java/com/javabunga/springbootexample/web/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.javabunga.springbootexample.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 public class HelloWorldController {
@@ -12,4 +15,14 @@ public class HelloWorldController {
         return "Hello " + name + "!";
     }
 
+    @RequestMapping("/uppercase")
+    public String uppercase(@RequestParam(value = "name") String name) { return name.toUpperCase(); }
+
+    @GetMapping("/random")
+    public int uppercase(@RequestParam(value = "min") Integer min, @RequestParam(value = "max") Integer max) {
+        final Random random = new Random();
+        return random.ints(min, max)
+                .findFirst()
+                .getAsInt();
+    }
 }
